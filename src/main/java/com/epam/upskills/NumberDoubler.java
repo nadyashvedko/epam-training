3009bf519db63a9ad36ac48db7d4cdf6b82c2bb6
Nadzeya Molchan <nadzeya_molchan@epam.com>
2020-09-18 00:18:51 +0300

    add number doubler

---
+++ src/main/java/com/epam/upskills/NumberDoubler.java
@@ -0,0 +1,21 @@
+package com.epam.upskills;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberDoubler {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please, enter any number: ");
+        while (true) {
+            try {
+                double number = Double.parseDouble(reader.readLine());
+                System.out.println(number * 2);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("This is not a number. Please, enter valid data");
+            }
+        }
+    }
+}
